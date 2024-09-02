4d66408e2f0e49179b2ecacf7b0754be3d993496
gadisom <jeongwon.kim@gmail.com>
2024-09-02 11:07:33 +0900

    Add : Landmarks

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 김정원 on 9/2/24.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello SwiftUI!")
+    }
+}
+    
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김정원 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
