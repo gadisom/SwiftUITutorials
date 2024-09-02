0f9d9f31ae3b6f51ae00a356b447f3a8e1383f31
gadisom <jeongwon.kim@gmail.com>
2024-09-02 11:49:32 +0900

    section 3

---
+++ Landmarks/Landmarks/LandmarkRow.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by 김정원 on 9/2/24.
+//
+
+import SwiftUI
+
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+
+#Preview {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
+}
